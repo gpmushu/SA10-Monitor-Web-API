efa7421bb290586bd835102b26aec37b991796e1
gpmushu@gmail.com <gpmushu@gmail.com>
2019-03-04 08:35:58 -0600

    Add project files.

---
+++ DatabaseAccess2/DatabaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess2
+{
+    public class DatabaseRepository
+    {
+        SA10_Monitor_DatabaseEntities3 db = new SA10_Monitor_DatabaseEntities3();
+
+        public IQueryable<Software> GetAllDatabase()
+        {
+            return db.Softwares;
+        }
+        public IQueryable<Software> GetAllDatabase(string id)
+        {
+            return db.Softwares.Where(a => a.AssetID == id).Select(e => e);
+        }
+    }
+}
---
+++ SA10MonitorWebAPI/Controllers/DatabaseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Sql;
+using SA10MonitorDatabaseAccess;
+using Newtonsoft.Json;
+
+
+namespace SA10MonitorWebAPI.Controllers
+{
+    public class DatabaseController : ApiController
+    {
+        SA10_Monitor_DatabaseEntities db = new SA10_Monitor_DatabaseEntities();
+
+        [HttpGet]
+        public IEnumerable<AssetStatus> Get()
+        {
+            return db.AssetStatus.ToList();
+        }
+
+        [HttpGet]
+        public IEnumerable<AssetStatus> GetbyAssetID(string id)
+        {
+            return db.AssetStatus.Where(e => e.assetID == id).ToList();
+        }
+
+        [HttpPost]
+        public void PostDatabase([FromBody] AssetStatus request)
+        {
+            db.AssetStatus.Add(request);
+            db.SaveChanges();
+        }
+    }
+}
